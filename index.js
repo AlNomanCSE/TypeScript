21a1c70f1c55e4a72a2945bb2c1e2f3892a75d25
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-02-26 21:12:41 +0600

    declare object but not inisialize,inisialize after

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log("Hello");
+let friends = ['al', 'Noman'];
+console.log(friends.push(123));
+const studen = {};
+studen.name = "Nikmasn";
+console.log(studen.name);
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+let friends: (string | number | boolean)[] = ['al', 'Noman'];
+console.log(friends.push(123));
+
+
+const studen: {
+    [ObjectKey: string]: string
+}
+    = {}
+
+studen.name = "Nikmasn";
+console.log(studen.name);
