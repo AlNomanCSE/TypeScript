f7d0c96477d8c2ad4a1f7a0e1af89f16f2bfe69f
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-02-27 21:50:24 +0600

    basic of TS

--- index.js
+++ index.js
@@ -15,3 +15,31 @@ console.log(add(12, 12));
 let value = add(12, 13);
 let valueINnumber = value;
 console.log(valueINnumber.toString());
+// enum
+var Days;
+(function (Days) {
+    Days[Days["monday"] = 0] = "monday";
+    Days[Days["tuesday"] = 1] = "tuesday";
+    Days[Days["wednesday"] = 2] = "wednesday";
+    Days[Days["thuyrsday"] = 3] = "thuyrsday";
+    Days[Days["friday"] = 4] = "friday";
+    Days[Days["saturday"] = 5] = "saturday";
+    Days[Days["sunday"] = 6] = "sunday";
+})(Days || (Days = {}));
+console.log(Days.monday);
+let address = 'khulna';
+let password = 111231;
+console.log(password.toFixed(3));
+let Order_1 = {
+    title: "Mr.",
+    price: 1234,
+    status: "Merried"
+};
+// string literal
+let userName;
+userName = "LOve";
+console.log(typeof userName);
+function optionalPermeter(a, b, c) {
+    return a + b;
+}
+console.log(optionalPermeter(100, -12, true));
--- index.ts
+++ index.ts
@@ -29,3 +29,42 @@ let value: unknown = add(12, 13);
 let valueINnumber: number = value as number;
 console.log(valueINnumber.toString());
 
+// enum
+enum Days {
+    monday, tuesday, wednesday, thuyrsday, friday, saturday, sunday
+}
+
+console.log(Days.monday);
+
+
+// aliases
+type xyz = string;
+let address: xyz = 'khulna'
+type mixed = string | number;
+let password: (mixed) = 111231;
+console.log(password.toFixed(3));
+
+type order = {
+    title: string;
+    price: number;
+    status: string
+}
+let Order_1: order = {
+    title: "Mr.",
+    price: 1234,
+    status: "Merried"
+}
+
+
+
+// string literal
+let userName: "Noman" | "Tanvir" | 'LOve';
+userName = "LOve"
+console.log(typeof userName);
+
+
+function optionalPermeter(a: number, b: number, c?: boolean): number {
+    return a + b;
+}
+
+console.log(optionalPermeter(100, -12, true));
